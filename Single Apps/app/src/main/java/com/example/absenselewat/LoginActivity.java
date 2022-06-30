558d396f3022ae759d4e5c73a59f83cc672639f9
aarioc97 <aarioc97@gmail.com>
2022-06-30 18:23:15 +0700

    Menyambungkan Mobile App dengan Firebase.
    Firebase sebagai basis data disambungkan dengan Mobile App, walaupun belum dapat melakukan insert, update, atau delete.

--- Single Apps/app/src/main/java/com/example/absenselewat/LoginActivity.java
+++ Single Apps/app/src/main/java/com/example/absenselewat/LoginActivity.java
@@ -6,15 +6,30 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity implements OnClickListener {
 
     Button login;
+    FirebaseAuth auth;
+    FirebaseDatabase databaseInstance;
+    DatabaseReference database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+        this.auth = FirebaseAuth.getInstance();
+        this.databaseInstance = FirebaseDatabase.getInstance();
+
+        if (this.auth.getCurrentUser() != null) {
+            startActivity(new Intent(this, AbsenActivity.class));
+        }
+
         setContentView(R.layout.activity_login);
 
         this.login = findViewById(R.id.login_button);
